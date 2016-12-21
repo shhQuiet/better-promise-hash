4b1620bc52397d28a6e62180484a44a3556434ed
Steve Horne <sshorne@dsthealthsolutions.com>
2016-12-21 15:10:24 -0600

    first try

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # better-promise-hash
 A better implementation of a hash function for Promise
+
+**Sometimes you feel like a nut, sometimes you don't**
+
+This modules adds a `hash` function to `Promise`.  This allows you to do things like this:
+
+    let object= {
+      one: new Promise((resolve,reject) => {
+          resolve('somePromiseValue');
+        }),
+      two: 'someOtherValue'
+    };
+
+    Promise.hash(object).then((result) => {
+      console.log(result.one); // echos "somePromiseValue"
+      console.log(result.two); // echos "someOtherValue"
+    });
+
+Hope that helps!
---
+++ test/test.js
@@ -0,0 +1,82 @@
+//'use strict';
+
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+require('../lib/better-promise-hash');
+
+function makeGood(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 1, value);
+  });
+}
+
+function makeBad(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(reject, 1, value);
+  });
+}
+
+function makeValue(value) {
+  return {
+    value: value
+  };
+}
+
+describe('better-promise-hash', () => {
+  it('Promise#hash() returns a promise', () => {
+    const result = Promise.hash({
+      one: makeGood(1)
+    }).then((results) => {}, (rejection) => {});
+    expect(result).to.be.instanceof(Promise);
+  });
+
+  it('Promise#hash() works with promises', (done) => {
+    const promises = {
+      one: makeGood('one'),
+      two: makeGood('two')
+    };
+    Promise.hash(promises).then((results) => {
+      expect(Object.keys(results).length).to.equal(2);
+      expect(results.one).to.equal('one');
+      done();
+    }, (rejection) => {
+      assert.fail("promise rejected!", rejection);
+    }).catch((exc) => {
+      assert.fail("Exception thrown!", exc);
+    });
+  });
+
+  it("Promise#hash() works with nonPromises", (done) => {
+    const nonPromises = {
+      one: makeValue('one'),
+      two: makeValue('two')
+    };
+    Promise.hash(nonPromises).then((results) => {
+      expect(Object.keys(results).length).to.equal(2);
+      expect(results.one.value).to.equal('one');
+      done();
+    }, (rejection) => {
+      assert.fail("promise rejected!", rejection);
+    }).catch((exc) => {
+      assert.fail("Exception thrown!", exc);
+    });
+  });
+
+  it("Promise#hash() works with a mix", (done) => {
+    const nonPromises = {
+      one: makeGood('one'),
+      two: makeValue('two')
+    };
+    Promise.hash(nonPromises).then((results) => {
+      expect(Object.keys(results).length).to.equal(2);
+      expect(results.one).to.equal('one');
+      expect(results.two.value).to.equal('two');
+      done();
+    }, (rejection) => {
+      assert.fail("promise rejected!", rejection);
+    }).catch((exc) => {
+      assert.fail("Exception thrown!", exc);
+    });
+  });
+});
